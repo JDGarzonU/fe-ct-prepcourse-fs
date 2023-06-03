cffbdabe567e878b70ca97f4c8d795be255d3078
JDGarzonU <jgarzon0269@gmail.com>
2023-06-02 19:25:52 -0500

    Homework JavaScript 03

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = "Johan";
+const nuevoString = "Johan David";
 
 // Crea una variable de tipo number.
 const nuevoNumero = 10;
